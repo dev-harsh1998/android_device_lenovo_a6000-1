342eb92454cd2e6ec8285d1ffc88453a9c67064b
dev-harsh1998 <harshit.india@linaro.org>
2018-03-04 13:40:31 +0530

    a6000: use our own powerhal

--- power/Android.mk
+++ power/Android.mk
@@ -1,22 +1,28 @@
-LOCAL_PATH := $(call my-dir)
+#
+# Copyright 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+LOCAL_PATH:= $(call my-dir)
+ifeq ($(TARGET_POWERHAL_VARIANT),lenovo) # Add check
 include $(CLEAR_VARS)
-
+LOCAL_C_INCLUDES := $(TARGET_POWERHAL_HEADER_PATH)
+LOCAL_SRC_FILES := power.c
+LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c \
-                   metadata-parser.c \
-                   utils.c \
-                   list.c \
-                   hint-data.c \
-                   power-8916.c
-
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
-endif
-
-LOCAL_MODULE := power.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := power.a6000
+LOCAL_PROPRIETARY_MODULE      := true
 include $(BUILD_SHARED_LIBRARY)
+endif # check complete
